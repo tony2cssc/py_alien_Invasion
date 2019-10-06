fa405aa8bf9a1ac481d649eb50ddb696311c4e91
tony2cssc <jijing.guo@gmail.com>
2019-10-06 20:19:16 +0800

    add fullscreen mode, but commented

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,6 +15,11 @@ class AlienInvasion(object):
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+
+        # Fullscreen mode
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
